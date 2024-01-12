9b48e27340f0c47887b81a60c866e3f12733a259
Raphael Collette <colletteraphael@gmail.com>
2024-01-12 18:40:32 -0500

    commit du 12 janvier

---
+++ dice.cpp
@@ -0,0 +1,30 @@
+#include "dice.hpp"
+
+Dice::Dice(){
+    this->nDice = 4; //nombre de des lances dans le jeu (toujours 4)
+}
+
+vector<vector<int>> Dice::checkCombinations(vector<int> results){
+    vector<vector<int>> combinations;
+    for (int i = 1; i < nDice; i++){
+        colonne1 = results[0] + results[i];
+        vector<int> choix;
+        if(i == 1){
+            colonne2 = results[2]+results[3];
+            choix.push_back(colonne1);
+            choix.push_back(colonne2);
+        }
+        else if (i == 2){
+            colonne2 = results[1]+results[3];
+            choix.push_back(colonne1);
+            choix.push_back(colonne2);
+        }
+        else{
+            colonne2 = results[1] + results[2];
+            choix.push_back(colonne1);
+            choix.push_back(colonne2);
+        }
+
+        combinations.push_back(choix);       
+    }
+}
